b6b9dd735cbb2a7e5e032619d191cde89659ebc9
Maria GG <mggrabwosky@gmail.com>
2026-09-19 12:21:01 +0200

    Enunciado

---
+++ ejercicio3.js
@@ -0,0 +1,9 @@
+/*3. Realizar un programa que calcule el salario de un trabajador, para ello pediremos el 
+número de horas trabajadas y el precio de la hora. Si el trabajador trabajas más de 40 
+horas semanales, se considera que trabaja horas extras. Las horas extras se pagan a 1.5 
+veces la hora normal. 
+a. Modifica el programa para que la operación se realice para cinco trabajadores. 
+b. Modifica el programa anterior para que el trabajador introduzca también su 
+nombre y dirección que mostraremos junto con su salario. 
+c. Modifica el programa anterior para que la salida se muestre en nuestro documento 
+html y también en la consola. */
